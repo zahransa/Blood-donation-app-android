6177fffa4375f97d445d2598de16a811398d2d77
SaeedZahran <saeedzahran@hotmail.com>
2021-11-19 15:26:06 +0100

    Cardview

---
+++ app/src/main/java/com/example/bda2/SelectRegisterationActivity.java
@@ -0,0 +1,14 @@
+package com.example.bda2;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class SelectRegisterationActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_select_registeration);
+    }
+}
